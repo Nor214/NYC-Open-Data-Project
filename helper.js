fa6b627ecf9f7dff30fde55cda1f4b863b0a46ad
Nor214 <NorrisR4@nycstudents.net>
2026-05-29 13:32:52 +0000

    card

--- helper.js
+++ helper.js
@@ -3,13 +3,16 @@ function get(id){
 }
 
 function card(info){
-  build = `<div class="fitted card">
+  build = `<div class="card line">
               <h2>${info.year}</h2>
               <h4>${info.leading_cause}</h4>
+              <h4>Deaths due to cause: ${info.deaths}</h4>
               <hr>
-              <h4>${info.sex}</h4>
-              <h4>${info.race_ethnicity}</h4>
-              <h4>${info.death_rate}</h4>
+              <h4>Victim's Sex: ${info.sex}</h4>
+              <h4>Victim's Ethnicity: ${info.race_ethnicity}</h4>
+              <hr>
+              <h4>Death Rate: ${info.death_rate}</h4>
+              <h4>Age adjusted Death Rate: ${info.age_adjusted_death_rate}</h4>
            </div>`;
   return build;
 }
--- script.js
+++ script.js
@@ -1,12 +1,32 @@
+let data;
 async function init(){
     let link = "data.json"
     info = await fetch(link);
     data = await info.json();
 
-    output = get("output");
+    let output = get("output");
 
     let build = ""
     for(let i = 0; i < data.length; i += 1){
-      build += "card(data)"
+      let death = data[i]
+      build += card(death)
     }
+
+    output.innerHTML = build
 }
+
+function filterbyyear(){
+
+    let output = get("output");
+    let year = get("yeardrop.value");
+
+    let build = ""
+    if(year == "twentyfive"){
+      for(let i = 0; i < data.length; i += 1){
+        let death = data[i]
+        build += card(death)
+      }
+    }  
+    output.innerHTML = build
+}
+
